3dfdc557b7cbc5fa017fc95928d254cc3e304c3a
DeveloperRyou <DeveloperRyou@gmail.com>
2024-02-16 14:45:27 +0900

    FIX: push FCM 함수 작성

--- App.tsx
+++ App.tsx
@@ -1,11 +1,53 @@
-import { Fragment } from "react";
-import { Dimensions, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { Fragment, useEffect, useState } from "react";
+import {
+  Dimensions,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import WebView from "react-native-webview";
+import fcmService from "./utils/fcm/service";
+import localNotificationService from "./utils/noti/service";
+import Clipboard from "@react-native-clipboard/clipboard";
 
 const deviceHeight = Dimensions.get("window").height;
 const deviceWidth = Dimensions.get("window").width;
 
 export default function App() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    fcmService.registerAppWithFCM();
+    fcmService.register(onRegister, onNotification, onOpenNotification);
+    localNotificationService.configure(onOpenNotification);
+  }, []);
+
+  const onRegister = (tk: string) => {
+    console.log("[App] onRegister : token :", tk);
+    if (tk) setToken(tk);
+  };
+
+  const onNotification = (notify: any) => {
+    console.log("[App] onNotification : notify :", notify);
+    const options = {
+      soundName: "default",
+      playSound: true,
+    };
+    localNotificationService.showNotification({
+      ...notify,
+      ...options,
+    });
+  };
+
+  const onOpenNotification = (notify: any) => {
+    console.log("[App] onOpenNotification : notify :", notify);
+  };
+
+  const onCopy = () => {
+    Clipboard.setString(token);
+  };
   return (
     <Fragment>
       <StatusBar />
@@ -15,6 +57,9 @@ export default function App() {
           source={{ uri: "https://gage.eolluga.com" }}
         />
       </SafeAreaView>
+      <TouchableOpacity onPress={onCopy}>
+        <Text>{token || "unknown token"}</Text>
+      </TouchableOpacity>
     </Fragment>
   );
 }
---
+++ utils/_const.ts
@@ -0,0 +1,4 @@
+import messaging from "@react-native-firebase/messaging";
+
+const messageModule = messaging();
+export { messageModule };
