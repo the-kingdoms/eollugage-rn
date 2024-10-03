d0665cecfa546f019e871835a87d444425bb3548
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-10-03 23:59:02 +0900

    chore: reactotron 사용 설정

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import CustomStatusBar from "@components/CustomStatusBar";
 import WebviewContainer from "@components/container/WebViewContainer";
 import { Fragment } from "react";
 
+if (__DEV__) {
+  require("./ReactotronConfig");
+}
+
 export default function App() {
   return (
     <Fragment>
