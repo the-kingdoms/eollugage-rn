3cbd4809e4887c08db1de46625dd7bc0a3a37387
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-10-28 02:46:27 +0900

    refactor: 로그인 여부 useeffect로 관리

--- navigators/AppBase.tsx
+++ navigators/AppBase.tsx
@@ -3,17 +3,23 @@ import JoinWebViewContainer from "pages/JoinWebViewContainer";
 import { useAtom } from "jotai";
 import { pathnameAtom, storeIdAtom } from "datas/atoms";
 import { getStoreId } from "@utils/parsePathname";
+import { useEffect, useState } from "react";
 
 export default function AppBase() {
   const [pathname] = useAtom(pathnameAtom);
-  const [, setStoreId] = useAtom(storeIdAtom);
+  const [storeId, setStoreId] = useAtom(storeIdAtom);
 
   const currentStoreId = getStoreId(pathname);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   console.log("currentStoreId:", currentStoreId);
 
-  if (currentStoreId.length > 0 && currentStoreId !== "join") {
-    setStoreId(currentStoreId);
-    return <BottomTabNav />;
-  }
+  useEffect(() => {
+    if (storeId !== currentStoreId) setStoreId(currentStoreId);
+
+    if (currentStoreId && currentStoreId.length > 0 && currentStoreId !== "join") setIsLoggedIn(true);
+    else setIsLoggedIn(false);
+  }, [currentStoreId]);
+
+  if (isLoggedIn) return <BottomTabNav />;
   return <JoinWebViewContainer />;
 }
